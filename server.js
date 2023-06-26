59eaa9dab2824328b9049bd9e75a1995872f5b76
Derick Stadler <derickstadler@gmail.com>
2023-06-26 00:12:53 -0600

    updated functionality changes also commiting the current bag state fixing soon

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const util = require("util");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const { table } = require("console");
+const { title } = require("process");
 
 const db = mysql.createConnection(
   {
@@ -56,6 +57,13 @@ const takeAction = (action) => {
     case "Add A Department":
       addDepartment();
       break;
+    case "Add A Role":
+      addRole();
+      break;
+    case "Add An Employee":
+      addEmployee();
+      // getRoles();
+      break;
     default:
       console.log("Sorry we couldnt process your request");
   }
@@ -92,11 +100,118 @@ const addDepartment = async () => {
     {
       type: "input",
       name: "department",
-      message: "What Department would you like to add?"
+      message: "What Department would you like to add?",
     },
-  ]
+  ];
 
   const addDepart = await inquirer.prompt(departPrompt);
-  const 
+  // console.log(addDepart);
+  const newDepart = addDepart.department;
+  // console.log(newDepart);
+  const insertDepart = await query(
+    `INSERT INTO departments (name) VALUES ('${newDepart}')`
+  );
+  console.log(`Added ${newDepart} to the database.`);
+  await handleActions();
+};
+
+const addRole = async () => {
+  const departArr = [];
+  const departList = await query("SELECT name FROM departments");
+  console.log(departList);
+  for (let department of departList) {
+    for (let name in department) {
+      departArr.push(`${department[name]}`);
+    }
+  }
+  console.log(departArr);
+  const rolePrompt = [
+    {
+      type: "input",
+      name: "role",
+      message: "What Role would you like to add?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the salary of the new role?",
+    },
+    {
+      type: "list",
+      name: "department",
+      message: "What department does the role fall under?",
+      choices: departArr,
+    },
+  ];
+  const addRole = await inquirer.prompt(rolePrompt);
+
+  const insertRole = await query(
+    `INSERT INTO roles (title, salary, department_id) VALUES (${addRole.role}, ${addRole.salary}, SELECT departments.id FROM departments WHERE name = "${addRole.department}")`
+  );
+  console.log(`Added ${addRole.role} role to the Database.`);
+  await handleActions();
+};
+
+const addEmployee = async () => {
+  const rolesArr = [];
+  const rolesList = await query("SELECT roles.title FROM roles");
+  // console.log(rolesList);
+  for (let roles of rolesList) {
+    for (let title in roles) {
+      // console.log(`${roles[title]}`);
+      rolesArr.push(`${roles[title]}`);
+    }
+    // console.log(rolesArr);
+  }
+  const manageArr = [];
+  const manageList = await query(
+    "SELECT CONCAT(first_name, ' ', last_name) AS employee FROM employees"
+  );
+  console.log(manageList);
+  for (let managers of manageList) {
+    for (let name in managers) {
+      manageArr.push(`${managers[name]}`);
+    }
+  }
+  console.log(manageArr);
+
+  const employeePrompt = [
+    {
+      type: "input",
+      name: "first",
+      message: "Whats the first name of the new employee?",
+    },
+    {
+      type: "input",
+      name: "last",
+      message: "Whats the last name of the new employee?",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the new employees role?",
+      choices: rolesArr,
+    },
+    {
+      type: "list",
+      name: "manager",
+      message: "Who is the new employees manager?",
+      choices: manageArr,
+    },
+  ];
+  const addEmploy = await inquirer.prompt(employeePrompt);
+  console.log(addEmploy.manager.split(" "));
+  const fullName = addEmploy.manager.split(" ");
+  const firstName = fullName[0];
+  const lastName = fullName[1];
+  console.log(firstName, lastName);
+  const insertEmploy = await query(`
+  INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (${addEmploy.first}, ${addEmploy.last}, SELECT roles.id FROM roles WHERE title = "${addEmploy.role}", SELECT employees.id FROM employees WHERE first_name = "${firstName}" AND last_name = "${lastName}")
+  `);
+
+  console.log(
+    `Added new employee ${addEmploy.first} ${addEmploy.last} to the database`
+  );
+  await handleActions();
 };
 // functions thatll handle the updating tables -- with emdedded inquirer
